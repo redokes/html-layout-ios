0d64b1589256b6a54aca07846839778dcc01ae79
Wes Okes <wokes@lamppostgroup.com>
2012-08-17 11:58:53 -0400

    * Added string until category

---
+++ html-layout-ios/Categories/NSString+Util.m
@@ -0,0 +1,23 @@
+//
+//  NSString+Util.m
+//  html-layout-ios
+//
+//  Created by Wes Okes on 8/17/12.
+//  Copyright (c) 2012 Jared Lewis. All rights reserved.
+//
+
+#import "NSString+Util.h"
+
+@implementation NSString (Util)
+
+- (BOOL)contains:(NSString *)subStr
+{
+    if ([self rangeOfString:subStr].location == NSNotFound) {
+        return NO;
+    }
+    else {
+        return YES;
+    }
+}
+
+@end

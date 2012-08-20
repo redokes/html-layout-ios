2b6ab9cdb80340e24670cee8dfdcf42c90a5dbd5
Jared Lewis <jlewis@lamppostgroup.com>
2012-08-20 10:29:54 -0400

    * Added new build target

---
+++ libHtmlLayoutIOS/libHtmlLayoutIOS.h
@@ -0,0 +1,13 @@
+//
+//  libHtmlLayoutIOS.h
+//  libHtmlLayoutIOS
+//
+//  Created by Jared Lewis on 8/20/12.
+//  Copyright (c) 2012 Jared Lewis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libHtmlLayoutIOS : NSObject
+
+@end
---
+++ libHtmlLayoutIOS/libHtmlLayoutIOS.m
@@ -0,0 +1,13 @@
+//
+//  libHtmlLayoutIOS.m
+//  libHtmlLayoutIOS
+//
+//  Created by Jared Lewis on 8/20/12.
+//  Copyright (c) 2012 Jared Lewis. All rights reserved.
+//
+
+#import "libHtmlLayoutIOS.h"
+
+@implementation libHtmlLayoutIOS
+
+@end
